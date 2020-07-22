02fc9640d1bcb583d60553303403594799dda7ce
Sylvain Laporte <sylvain.laporte.2@umontreal.ca>
2020-07-22 16:46:47 -0400

    Taking turns

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -28,13 +28,17 @@ function Square(props) {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,  // to allow taking turn
       };
     }
 
     handleClick(i) {
       const squares = this.state.squares.slice(); // .slice() to create a copy of the array
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      });
     }
 
     renderSquare(i) {
@@ -47,7 +51,7 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
