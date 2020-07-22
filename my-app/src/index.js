24f4fe19a341a631dcaf5a2b2f6f467954d658c5
Sylvain Laporte <sylvain.laporte.2@umontreal.ca>
2020-07-22 16:30:43 -0400

    Lifting state up

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,28 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        value: null,
-      };
-    }
-
     render() {
       return (
         <button
           className="square"
-          onClick={() => this.setState({value: 'X'})}
+          onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
   }
   
   class Board extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
+    handleClick(i) {
+      const squares = this.state.squares.slice(); // .slice() to create a copy of the array
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
-      return <Square value={i}/>;
+      return (
+        <Square
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );  // Parenthesis so that JS doesn't insert a semicolon after return.
     }
   
     render() {
